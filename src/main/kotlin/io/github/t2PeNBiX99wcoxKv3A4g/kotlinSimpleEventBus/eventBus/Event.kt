f6562d8aae07338f1894cd8afe5ede69d403f653
yukiyosanako <179217493+T2PeNBiX99wcoxKv3A4g@users.noreply.github.com>
2025-06-15 23:19:57 +0800

    fix: Change `Event` class to abstract class

--- src/main/kotlin/io/github/t2PeNBiX99wcoxKv3A4g/kotlinSimpleEventBus/eventBus/Event.kt
+++ src/main/kotlin/io/github/t2PeNBiX99wcoxKv3A4g/kotlinSimpleEventBus/eventBus/Event.kt
@@ -1,3 +1,18 @@
+@file:Suppress("unused")
+
 package io.github.t2PeNBiX99wcoxKv3A4g.kotlinSimpleEventBus.eventBus
 
-interface Event
+abstract class Event {
+    private var _isCancel = false
+
+    fun cancel() {
+        isCancel = true
+    }
+
+    @Suppress("MemberVisibilityCanBePrivate")
+    var isCancel: Boolean
+        get() = _isCancel
+        private set(value) {
+            _isCancel = value
+        }
+}
