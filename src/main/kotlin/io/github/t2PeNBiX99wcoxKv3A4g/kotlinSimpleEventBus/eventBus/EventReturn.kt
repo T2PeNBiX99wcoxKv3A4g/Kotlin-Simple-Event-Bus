6b4cadbec79c3e533d47b6fd6490086dc1e46a0e
yukiyosanako <179217493+T2PeNBiX99wcoxKv3A4g@users.noreply.github.com>
2025-06-19 22:20:42 +0800

    fix(EventReturn): Make `EventReturn` more like map

--- src/main/kotlin/io/github/t2PeNBiX99wcoxKv3A4g/kotlinSimpleEventBus/eventBus/EventReturn.kt
+++ src/main/kotlin/io/github/t2PeNBiX99wcoxKv3A4g/kotlinSimpleEventBus/eventBus/EventReturn.kt
@@ -1,25 +1,64 @@
-@file:Suppress("unused", "UNUSED_PARAMETER")
+@file:Suppress("unused")
 
 package io.github.t2PeNBiX99wcoxKv3A4g.kotlinSimpleEventBus.eventBus
 
-class EventReturn<T : Any>(private val data: Map<EventReturnData, T?>) {
+class EventReturn<T : Any>(private val data: Map<EventReturnData, T?>) : Map<EventReturnData, T?> {
     /**
      * Get first value in return values
+     *
+     * Return **null** if data is empty
      */
-    fun first() = data.entries.minByOrNull { it.key.order }!!
+    fun first() = data.entries.minByOrNull { it.key.order }
 
     /**
      * Get last value in return values
+     *
+     * Return **null** if data is empty
      */
-    fun last() = data.entries.maxByOrNull { it.key.order }!!
-
-    /**
-     * Performs the given [action] on each entry.
-     */
-    fun forEach(action: (Map.Entry<EventReturnData, T?>) -> Unit) = data::forEach
+    fun last() = data.entries.maxByOrNull { it.key.order }
 
     /**
      * Get the map
      */
-    fun asMap() = data
+    fun toMap() = data
+
+    /**
+     * Returns a read-only [Set] of all key/value pairs in this map.
+     */
+    override val entries: Set<Map.Entry<EventReturnData, T?>> = data.entries
+
+    /**
+     * Returns a read-only [Set] of all keys in this map.
+     */
+    override val keys: Set<EventReturnData> = data.keys
+
+    /**
+     * Returns the number of key/value pairs in the map.
+     */
+    override val size: Int = data.size
+
+    /**
+     * Returns a read-only [Collection] of all values in this map. Note that this collection may contain duplicate values.
+     */
+    override val values: Collection<T?> = data.values
+
+    /**
+     * Returns the value corresponding to the given [key], or `null` if such a key is not present in the map.
+     */
+    override fun get(key: EventReturnData): T? = data[key]
+
+    /**
+     * Returns `true` if the map maps one or more keys to the specified [value].
+     */
+    override fun containsValue(value: T?): Boolean = data.containsValue(value)
+
+    /**
+     * Returns `true` if the map contains the specified [key].
+     */
+    override fun containsKey(key: EventReturnData): Boolean = data.containsKey(key)
+
+    /**
+     * Returns `true` if the map is empty (contains no elements), `false` otherwise.
+     */
+    override fun isEmpty(): Boolean = data.isEmpty()
 }
